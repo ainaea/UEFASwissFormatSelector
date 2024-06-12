3b6fe2bcb1b2fb9dfcc81393ffb5603100598be4
Aina <Aina@LAPTOP-721RF3FI>
2024-06-12 11:45:52 +0100

    Added MatchDrawService with PotTeam method.

---
+++ Services/MatchDrawService.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using UEFASwissFormatSelector.Models;
+
+namespace UEFASwissFormatSelector.Services
+{
+    public class MatchDrawService
+    {
+        public IEnumerable<Pot> PotTeam(IEnumerable<ClubInScenarioInstance> clubsInScenarioInstance)
+        {
+            var scenario = new Scenario();
+            Pot[] pottedTeams = new Pot[scenario.NumberOfPot];
+            clubsInScenarioInstance = clubsInScenarioInstance.OrderByDescending( c => c.Ranking).ToList();
+            for (int i = 0; i < scenario.NumberOfPot; i++)
+            {
+                pottedTeams[i] = new Pot($"Pot {Enum.GetName(typeof(PotEnum), i) ?? i.ToString()}", scenario.NumberOfTeamsPerPot);
+                var clubsInPot = clubsInScenarioInstance.Skip(i * scenario.NumberOfTeamsPerPot).Take(scenario.NumberOfTeamsPerPot).ToList();
+                pottedTeams[i].ClubsInPot = clubsInPot.Select( c => new ClubInPot(c.ClubId, pottedTeams[i].Id) { Club = c.Club} );
+            }
+            return pottedTeams;
+        }
+    }
+}
