b4d1f07f70660df5ff7d4545075dbe4ccec16e54
Aina <Aina@LAPTOP-721RF3FI>
2024-07-01 17:37:37 +0100

    Making the explore view retain prior data when adding new ones

--- ViewModels/ExploreScenarioInstanceViewModel.cs
+++ ViewModels/ExploreScenarioInstanceViewModel.cs
@@ -1,9 +1,18 @@
-﻿using UEFASwissFormatSelector.Models;
+﻿using System.Reflection;
+using UEFASwissFormatSelector.Models;
 
 namespace UEFASwissFormatSelector.ViewModels
 {
     public class ExploreScenarioInstanceViewModel: ScenarioInstance
     {
+        public bool NoClubInScenarioInstance { get=> this.ClubsInScenarioInstance == null || this.ClubsInScenarioInstance.Count() == 0 || this.ClubsInScenarioInstance.Any(c => c == null); }
+        public bool ClubsCanBePotted { get => !NoClubInScenarioInstance; }
+        public bool ClubsNotPotted { get=> Pots.Any(p => p == null) || Pots.Count() == 0; }
+        public bool ClubOpponentsCanBeGenerated { get => !ClubsNotPotted && ClubsCanBePotted; }
+        public bool ClubOpponentsNotGenerated { get=> !ClubsNotPotted && Pots.Any(c => c.ClubsInPot != null) && Opponents == null; }
+        public bool MatchupsCanBeDecided { get => !ClubOpponentsNotGenerated && ClubOpponentsCanBeGenerated; }
+        public bool MatchupsNotDecided { get=> Opponents != null && MatchUps == null; }
+        public bool CanDisplayMatchUp { get=> MatchUps != null; }
         //Teams
         //Pots
         //Matches
