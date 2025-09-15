df7ab8a417343f05bbd99b2524c80b20341fe5c1
Aina <Aina@LAPTOP-721RF3FI>
2025-09-15 13:08:16 +0100

    Saved and repushed

--- Services/AppDbContext.cs
+++ Services/AppDbContext.cs
@@ -1,6 +1,21 @@
-﻿namespace UEFASwissFormatSelector.Services
+﻿using Microsoft.EntityFrameworkCore;
+using UEFASwissFormatSelector.Models;
+
+namespace UEFASwissFormatSelector.Services
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Pot> Pots { get; set; }
+        public DbSet<ClubInPot> ClubsInPot { get; set; }
+        public DbSet<ClubInScenarioInstance> ClubsInScenarioInstance { get; set; }
+        public DbSet<Scenario> Scenarios { get; set; }
+        public DbSet<ScenarioInstance> ScenarioInstances { get; set; }
+
     }
 }
