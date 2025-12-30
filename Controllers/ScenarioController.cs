128d48eb0bcaf2f7dbb2f62156d6dcf3630c35b9
Aina <Aina@LAPTOP-721RF3FI>
2025-12-30 19:33:17 +0100

    Implementation to prevent creation of scenario that  can never be fully matched-up

--- Controllers/ScenarioController.cs
+++ Controllers/ScenarioController.cs
@@ -28,10 +28,18 @@ namespace UEFASwissFormatSelector.Controllers
         {
             if (ModelState.IsValid)
             {
-                var similarScenario = repository.Scenarios.FirstOrDefault(s => s.NumberOfTeamsPerPot == scenario.NumberOfTeamsPerPot && s.NumberOfPot == scenario.NumberOfPot && s.NumberOfGamesPerPot == scenario.NumberOfGamesPerPot && s.HomeAndAwayPerOpponent == scenario.HomeAndAwayPerOpponent );
+                var similarScenario = repository.Scenarios.FirstOrDefault(s => s.NumberOfTeamsPerPot == scenario.NumberOfTeamsPerPot && s.NumberOfPot == scenario.NumberOfPot && s.NumberOfGamesPerPot == scenario.NumberOfGamesPerPot && s.HomeAndAwayPerOpponent == scenario.HomeAndAwayPerOpponent);
                 if (similarScenario != null)
                 {
                     ModelState.AddModelError("", "Similar scenario is already existing");
+                    return View(scenario);
+                }
+                if (scenario.NumberOfTeamsPerPot <= scenario.NumberOfGamesPerPot || scenario.NumberOfTeamsPerPot * scenario.NumberOfGamesPerPot % 2 != 0)
+                {
+                    if (scenario.NumberOfTeamsPerPot <= scenario.NumberOfGamesPerPot)
+                        ModelState.AddModelError("",$"It is impossible to get {scenario.NumberOfGamesPerPot} opponents from {scenario.NumberOfTeamsPerPot} teams in all pots");
+                    if (scenario.NumberOfTeamsPerPot * scenario.NumberOfGamesPerPot % 2 != 0)
+                        ModelState.AddModelError("", $"Can not create {(scenario.NumberOfGamesPerPot * scenario.NumberOfTeamsPerPot+1)/2} fixtures from {scenario.NumberOfGamesPerPot * scenario.NumberOfTeamsPerPot} team slots");
                     return View(scenario);
                 }
                 repository.Scenarios.Add(scenario);
